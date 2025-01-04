1ee116e3c34bf6c067bd46fa83b376f7b4a886b1
Batool Kazmi <kazmib6@gmail.com>
2025-01-04 10:43:50 -0500

    finished the user quiz page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/LogoWhite.svg";
 
+// find products in the navbar
+
 function NavBar() {
   return (
     <nav className="navbar">
-      <img src={Logo} alt="logo" className="w-20 h-auto" />
+      <NavLink to="/">
+        <img src={Logo} alt="logo" className="w-20 h-auto" />
+      </NavLink>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/test">Test</NavLink>
-      <NavLink to="/camera">Camera</NavLink>
+      <NavLink to="/test">Find Products</NavLink>
     </nav>
   );
 }
