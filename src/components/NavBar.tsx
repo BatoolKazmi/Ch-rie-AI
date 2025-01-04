558a783fd4b6632c8acb26a39bf7d21d98cdd905
Batool Kazmi <kazmib6@gmail.com>
2025-01-04 01:25:05 -0500

    added logo to navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/LogoWhite.svg";
 
 function NavBar() {
   return (
     <nav className="navbar">
+      <img src={Logo} alt="logo" className="w-20 h-auto" />
       <NavLink to="/">Home</NavLink>
       <NavLink to="/test">Test</NavLink>
     </nav>
