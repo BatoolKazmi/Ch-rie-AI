9c2abe908d34c8a9524504ef2cc9124b45f3c8fd
Batool Kazmi <kazmib6@gmail.com>
2025-01-08 23:42:30 -0500

    merged main and camera

--- src/components/routes/Test.tsx
+++ src/components/routes/Test.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import NavBar from "../NavBar"; // Ensure this path is correct
 
 function Test() {
   const [phase, setPhase] = useState(1);
--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,18 +1,14 @@
 import Home from "./Home";
 import Camera from "./Camera";
-<<<<<<< HEAD
 import Picture from "./Picture";
-=======
 import Products from "./Products";
 import Layout from "./Layout";
 import ErrorPage from "./ErrorPage";
 import Test from "./Test";
->>>>>>> f594de2b4bb398bdd6686098676f9c7688b3c2ab
 
 const routes = [
   {
     path: "/",
-<<<<<<< HEAD
     element: <Home />,
   },
   {
@@ -27,33 +23,32 @@ const routes = [
     path: "/picture",
     element: <Picture />,
   },
-=======
+  {
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Home />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
       },
       {
         path: "/test",
         element: <Test />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
       },
       {
         path: "/camera",
         element: <Camera />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
       },
       {
         path: "/products",
         element: <Products />,
-        errorElement: <ErrorPage />
-      }
-    ]
-  }
->>>>>>> f594de2b4bb398bdd6686098676f9c7688b3c2ab
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
 ];
 
 export default routes;
