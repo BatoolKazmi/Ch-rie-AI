47b8035500a67e15e52917331fa5e0abee5dcfe0
laygofiona <laygo.fiona@gmail.com>
2025-01-10 16:44:20 -0500

    Fixing deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: [
+        '@rollup/rollup-darwin-arm64',
+        '@rollup/rollup-linux-x64-gnu'
+      ]
+    }
+  }
 })
